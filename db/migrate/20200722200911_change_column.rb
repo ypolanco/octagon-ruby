a05b8ff288f4c7746f8f3af30fad7cb3229d28ce
Daehyun <dhc4192@gmail.com>
2020-07-22 16:23:43 -0400

    completed backend auth

--- db/migrate/20200722200911_change_column.rb
+++ db/migrate/20200722200911_change_column.rb
@@ -1,6 +1,6 @@
 class ChangeColumn < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :name, :username
+    rename_column :users, :name, :username
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,29 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_722_194_531) do
+ActiveRecord::Schema.define(version: 2020_07_22_200911) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'events', force: :cascade do |t|
-    t.string 'name'
-    t.date 'date'
-    t.time 'time'
-    t.string 'gate'
-    t.string 'section'
-    t.string 'row'
-    t.string 'seat'
-    t.string 'ticket_number'
-    t.string 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.time "time"
+    t.string "gate"
+    t.string "section"
+    t.string "row"
+    t.string "seat"
+    t.string "ticket_number"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
