073e168795b004759cd63936bd274e55b1d7dbce
Daehyun <dhc4192@gmail.com>
2020-07-23 16:00:21 -0400

    ignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Steps from "./components/Steps";
 import Qr from "./components/Qr"
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,44 +12,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_220150) do
-
+ActiveRecord::Schema.define(version: 20_200_722_220_150) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.date "date"
-    t.time "time"
-    t.string "gate"
-    t.string "section"
-    t.string "row"
-    t.string "seat"
-    t.string "ticket_number"
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "venue_id", null: false
-    t.index ["venue_id"], name: "index_events_on_venue_id"
+  create_table 'events', force: :cascade do |t|
+    t.string 'name'
+    t.date 'date'
+    t.time 'time'
+    t.string 'gate'
+    t.string 'section'
+    t.string 'row'
+    t.string 'seat'
+    t.string 'ticket_number'
+    t.string 'description'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'venue_id', null: false
+    t.index ['venue_id'], name: 'index_events_on_venue_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.string 'password_digest'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "venues", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.integer "zip_code"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'venues', force: :cascade do |t|
+    t.string 'name'
+    t.string 'address'
+    t.string 'city'
+    t.string 'state'
+    t.integer 'zip_code'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "events", "venues"
+  add_foreign_key 'events', 'venues'
 end
